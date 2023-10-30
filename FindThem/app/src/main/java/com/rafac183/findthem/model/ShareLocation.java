1085dc870c2c911b47375c393b0fe9946e52344e
rafac183 <rafaelantoniocordero2002@gmail.com>
2023-10-29 21:57:49 -0300

    Agregue parcialmente un punto para el Mapa

---
+++ FindThem/app/src/main/java/com/rafac183/findthem/model/ShareLocation.java
@@ -0,0 +1,39 @@
+package com.rafac183.findthem.model;
+
+public class ShareLocation {
+    String id, name;
+    double latitude, longitude;
+    public ShareLocation() {}
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
