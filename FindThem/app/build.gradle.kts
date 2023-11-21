58870cfe09e5591a0ca682a3feb11edf42f3ab58
rafac183 <rafaelantoniocordero2002@gmail.com>
2023-11-21 18:50:48 -0300

    Version Code

--- FindThem/app/build.gradle.kts
+++ FindThem/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.rafac183.findthem"
         minSdk = 23
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
